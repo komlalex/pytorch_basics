ea3c4f846ca8013a64fbbca170c9dace2f556e2e
Alexander Komla <kunalexander360@gmail.com>
2025-03-17 21:59:08 +0000

    Understading gradient descent

--- pytorch_basics.py
+++ pytorch_basics.py
@@ -175,7 +175,7 @@ The matrix obtained by passing the input data into the model is a set of predict
 initialized our model with random weights and biases. Obviously, we can't expect a randomly initialized model to work""" 
 diff = preds - targets
 loss = torch.sum(diff * diff) / diff.numel() 
-print(loss)
+#print(loss)
 
 """LOSS FUNCTION 
 Before we improve our model, we need to evaluate how well our model is performing. We can compare
@@ -194,4 +194,65 @@ torch.sum returns the sum of all the elements in a tensor
 torch.numel returns the number of elements in a tensors
 """
 loss = mse(preds, targets)
-print(loss)
+print(loss)  
+
+"""
+Here's how we can interpret the result: On average, each element in the prediction differs from the 
+actual target by the square root of the loss. And that's pretty bad, considering the numbers 
+we are trying to predict are themselves in the range 50 - 200. The result is called 
+the loss because it indicates how bad the model is at predicting the target values. It 
+represents information loss in the model: the lower the loss, the better the model. 
+"""
+
+"""COMPUTE GRADIENTS
+With PyTorch, we can automatically compute the gradients or derivativess of the loss w.r.t to the weights and biases because
+they have require_grad set to True. We'll see how this is useful in just a moment.
+"""
+# Compute gradients 
+loss.backward()
+
+"""The gradients are stored in the .grad property of the respective tensors. Not tht the derivatives of the loss w.r.t the weights matrix 
+is itself a matrix with the same dimensions""" 
+# Gradients for weigths 
+#print(w)
+#print(w.grad)
+
+"""Adjust weights and biases to reduce the loss
+The loss is a quadratic function of our weights and biases, and our objective is to find
+the set of weights where the loss is the lowest. An important insight from calculus is that the 
+gradient indicates the rate of change of the loss, i.e., the loss function's slope w.r.t the weights and biases. 
+If a gradient is positive: 
+* increasing the weight element's value slightly will increase the loss 
+* decreasing the weight element's value slightly will decrease the loss
+
+If the gradient is negative:
+* increasing the weight element's value slightly will decrease the loss 
+* decreasing the weight element's value slighly will increase the loss
+""" 
+#print(w)
+#print(w.grad)
+#print(w - w.grad * 1e-5)
+
+with torch.no_grad():
+    w -= w.grad * 1e-5
+    b -= b.grad * 1e-5  
+
+"""We multiply the gradients with a very small number (10^-5) in this case to ensure that we don't modify the 
+weights by a very large amount. We want to take a small step in the downhill direction of the gradient, not a giant leap. This number 
+is called the learning rate of the algorithm. 
+
+We use torch.no_grad to indicate to PyTorch that we shouldn't track, calculate, or modify 
+gradiets while updating the weights and biases""" 
+
+# Let's verify that the loss is actually lower 
+preds = model(inputs)
+loss = mse(preds, targets) 
+#print(loss) 
+
+"""Before we proceed, we reset the gradients to zero by invoking the .zero_grad() method. We need to 
+do this because PyTorch accumulates gradients. Otherwise, the next time we invoke .backward on the loss, the new gradients values are added to the existing gradients, which 
+may lead to unexpected results""" 
+
+w.grad.zero_()
+b.grad.zero_() 
+print(w.grad)
